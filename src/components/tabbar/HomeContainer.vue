96f582addbbda8b6d2ce946792f5eeb4282114a8
you <you@example.com>
2018-01-03 19:10:13 +0800

    aa

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        HomeContainer
+    <div class="">
+        <mt-swipe :auto="4000">
+        <mt-swipe-item>1</mt-swipe-item>
+        <mt-swipe-item>2</mt-swipe-item>
+        <mt-swipe-item>3</mt-swipe-item>
+        </mt-swipe>
     </div>
 </template>
 
@@ -8,15 +12,47 @@
     export default {
         data () {
             return {
+                 lunbotuList: []  // 保存轮播图的数组
                 
             }
         },
+         created() {
+            this.getLunbotu()
+        },
         methods:{
+            getLunbotu(){
+                this.$http.get('http://vue.studyit.io/api/getlunbo').then(result=>{
+                   if(result.body.status===0){
+                           this.lunbotuList = result.body.message;
+                    }else{
+                    
+                        Toast("加载轮播图失败……")
+                    } 
+                    
+                })
+
+            }
             
         }
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.mint-swipe {
+        height: 200px;
+
+        .mint-swipe-item {
+            &:nth-child(1) {
+                background-color: red;
+            }
+            &:nth-child(2) {
+                background-color: blue;
+            }
+            &:nth-child(3) {
+                background-color: cyan;
+            }
+           
+        }
+    }
     
 </style>
--- src/main.js
+++ src/main.js
@@ -6,10 +6,18 @@ import app from './App.vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter);
 import router from './router.js'
+//导入 路由的包
+import VueResource from 'vue-resource';
+//  安装路由
+Vue.use(VueResource);
 
 //头部
 import { Header } from 'mint-ui';
 Vue.component(Header.name, Header);
+//轮播图
+import { Swipe, SwipeItem } from 'mint-ui';
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 
 //底部
 import './lib/mui/css/mui.min.css'
