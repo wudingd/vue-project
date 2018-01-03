15161a73ac4b81e278321193013bd8c33fb3913b
you <you@example.com>
2018-01-03 20:27:23 +0800

    222

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -50,7 +50,7 @@
                            this.lunbotuList = result.body.message;
                     }else{
                     
-                        Toast("加载轮播图失败1112222……")
+                        Toast("加载轮播图失败……")
                     } 
                     
                 })
